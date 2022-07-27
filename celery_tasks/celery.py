bfc867b830fa438cb8a2bbc44d9e466eb1cbd670
Maurizio Santamicone <santamm@gmail.com>
2022-07-27 15:22:07 +0100

    Added gradio ui

--- celery_tasks/celery.py
+++ celery_tasks/celery.py
@@ -1,8 +1,20 @@
 from celery import Celery
+import os
+import json
+from pathlib import Path
 
-app = Celery('hf_model',
-  broker='pyamqp://guest@localhost//',
-  backend='redis://localhost',
+
+
+with open(Path(os.path.dirname(__file__)).parent/"config.json") as cfg_file:
+        worker_dict = json.load(cfg_file)
+
+celery_broker_url = worker_dict ["celery_broker_url"]
+celery_backend_url = worker_dict ["celery_backend_url"]
+
+
+app = Celery('netmind_model',
+  broker=celery_broker_url,
+  backend=celery_backend_url ,
   include=['celery_tasks.tasks']
 )
 
---
+++ ui.py
@@ -0,0 +1,66 @@
+import gradio as gr
+import requests
+import time
+import os
+
+#base_uri = os.environ("NETMIND_API_URI")
+
+# uri of the uvicorn/gunicorn ASGI web server
+base_uri = r'http://127.0.0.1:8000'
+
+
+def submit_task(text):
+  """
+  """
+  payload = {"data":text}
+  predict_task_uri = base_uri + '/andrea/predict'
+  task = requests.post(predict_task_uri, json=payload)
+  #print(f"Submitted task: {task.json()['task_id']}")
+  task_id = task.json()['task_id']
+
+  return task_id
+
+
+
+def retrieve_task_results(taskid):
+  """
+  """
+
+  predict_result_uri = base_uri + '/andrea/result/' + taskid
+  response = requests.get(predict_result_uri)
+  if response.status_code == 200:
+    result = response.json()
+    status = result['status']
+    summary = result['result']
+  else:
+    summary = "No luck."
+    status = "Failed"
+  return status, summary
+
+
+def submit_interactive(text, poll_interval=6, max_attempts=5):
+  """
+  
+  """
+  start = time.time()
+  taskid = submit_task(text)
+  attempts = 0
+  result = None
+  while attempts < max_attempts:
+    attempts += 1
+    status, result = retrieve_task_results(taskid)
+    if status=='Success':
+      elapsed = f"Inference time: {(time.time()-start):.4} secs."
+      return status, result, elapsed
+    time.sleep(poll_interval)
+  return "Failed", "Response timeout", ""
+
+
+gr.Interface(
+    fn=submit_interactive,
+    title="Text Summarizer by NetMind",
+    inputs=gr.Textbox(lines=20, label="Text"),
+    outputs=[gr.Textbox(label="Status"), gr.Textbox(label="Summary"), gr.HTML()]
+    #live=True
+).launch()
+
