9649973e1844430e69461f7b286b97bd5b931872
Maurizio Santamicone <santamm@gmail.com>
2022-08-25 11:32:26 +0200

    Fixed requirements.txt files

--- celery_tasks/ml_models/andrea-summarization/model.py
+++ celery_tasks/ml_models/andrea-summarization/model.py
@@ -1,5 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelWithLMHead, pipeline
-import joblib
+#import joblib
 import os
 from pathlib import Path
 import logging
@@ -29,42 +29,24 @@ class AndreaSummarize:
         self.model.load_state_dict(torch.load(self.model_name_or_path+"/pytorch_model.bin", map_location='cpu'))
         
         self.device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
-        self.model.to(self.device)
+        #self.model.to(self.device)
         self.model.eval()
-
-
-
-    @staticmethod
-    def _load_model_from_path(path):
-        # that will only work id the model has been persisted with joblib.dump()
-        #self.tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
-        #model = AutoModelForSeq2SeqLM.from_pretrained(self.model_name_or_path)
-        #self.model.load_state_dict(torch.load("pytorch_model.bin", map_location='cpu')
-        #print(self.model.load_state_dict(torch.load("celery_tasks/ml_models/andrea-summarization/pytorch_model.bin", map_location='cpu')))
-        #self.summarizer = pipeline("summarization", model=self.model, tokenizer=self.tokenizer)
-        logging.info("Loading model checkpoint from "+str(MODEL_PATH))
-        model = joblib.load(path)
-        return model
+        self.summarizer = pipeline(
+            "summarization", model=self.model, tokenizer=self.tokenizer, 
+            num_beams=5, do_sample=True, no_repeat_ngram_size=3,
+            max_length=1024,
+            device = 1 if torch.cuda.is_available() else -1,  # -1 means running on CPU
+            batch_size=1
+            )
 
     def predict(self, data):
         """
         
         """
 
-        tokens = self.tokenizer(data, return_tensors='pt', max_length=384, truncation=True, padding='max_length')
-        
-        output = self.model.generate(
-            tokens.input_ids,
-            num_beams=5,
-            do_sample=True,
-            top_k=0,
-            temperature=0.7,
-            num_return_sequences=1,
-            no_repeat_ngram_size=3,
-            remove_invalid_values=False,
-            early_stopping=True  # ?
-            )
-        prediction = self.tokenizer.decode(output[0], skip_special_tokens=True)
+        #tokens = self.tokenizer(data, return_tensors='pt', max_length=384, truncation=True, padding='max_length')
+        #prediction = self.tokenizer.decode(output[0], skip_special_tokens=True)
 
-        #prediction = self.model(data, truncation=True, num_beams=5)
+        prediction = self.summarizer(data, truncation=True)
         return prediction
+
--- start_celery.sh
+++ start_celery.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 ps auxww | grep 'celery_tasks worker' | awk '{print $2}' | xargs kill -9
 sleep 2
-celery -A celery_tasks worker -P eventlet -l INFO --detach --logfile celery.log &
+conda run -n summarize celery -A celery_tasks worker -P eventlet -Q Summarization -l INFO --detach --logfile celery.log &
+celery -A celery_tasks worker -P eventlet -Q Translation -l INFO --detach --logfile celery.log &
+conda run -n codegen celery -A celery_tasks worker -P eventlet -Q Generation -l INFO --detach --logfile celery.log &
+celery -A celery_tasks worker -P eventlet -Q celery -l INFO --detach --logfile celery.log &
 
+# If you need to use different virtual environments, use conda run -n <venv> celery --version
+ps -ef | grep celery
