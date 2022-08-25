3bf1c3fa0854bdc24a41deb4863aaf6ee35db2f1
Maurizio Santamicone <santamm@gmail.com>
2022-08-25 13:44:23 +0200

    Andrea model.py

--- celery_tasks/ml_models/andrea-summarization/model.py
+++ celery_tasks/ml_models/andrea-summarization/model.py
@@ -29,14 +29,13 @@ class AndreaSummarize:
         self.model.load_state_dict(torch.load(self.model_name_or_path+"/pytorch_model.bin", map_location='cpu'))
         
         self.device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
-        #self.model.to(self.device)
+        self.model.to(self.device)
         self.model.eval()
-        self.summarizer = pipeline(
-            "summarization", model=self.model, tokenizer=self.tokenizer, 
-            num_beams=5, do_sample=True, no_repeat_ngram_size=3,
+        self.summarizer = pipeline("summarization", model=self.model, tokenizer=self.tokenizer,
+            do_sample=True, no_repeat_ngram_size=3,
             max_length=1024,
-            device = 1 if torch.cuda.is_available() else -1,  # -1 means running on CPU
-            batch_size=1
+            batch_size=1,
+            device = 1 if torch.cuda.is_available() else -1  # -1 means running on CPU
             )
 
     def predict(self, data):
@@ -47,6 +46,6 @@ class AndreaSummarize:
         #tokens = self.tokenizer(data, return_tensors='pt', max_length=384, truncation=True, padding='max_length')
         #prediction = self.tokenizer.decode(output[0], skip_special_tokens=True)
 
-        prediction = self.summarizer(data, truncation=True)
+        prediction = self.summarizer(data, num_beams=5, truncation=True)
         return prediction
 
